1fe6fdcbd11e844f8d6457be91868abd769429d7
Ricky25119 <aarizmohammad92@gmail.com>
2025-07-08 10:52:13 +0100

    Create powershell.ps1

---
+++ powershell.ps1
@@ -0,0 +1,19 @@
+- task: PowerShell@2
+      displayName: 'Replace appsettings.json with selected environment file'
+      inputs:
+        targetType: 'inline'
+        script: |
+          $config = "$(buildConfiguration)"
+          Write-Host " customerName : $(buildConfiguration) "
+          $envFile = "appsettings.$config.json"
+          $projectDir = "$(Build.SourcesDirectory)/API/ASP.net_core_API"
+          $sourcePath = "$projectDir/$envFile"
+          $destPath = "$projectDir/appsettings.json"
+
+          if (Test-Path $sourcePath) {
+            Copy-Item -Path $sourcePath -Destination $destPath -Force
+            Write-Host " Replaced appsettings.json with $envFile"
+          } else {
+            Write-Error " File $envFile not found!"
+            exit 1
+          }
